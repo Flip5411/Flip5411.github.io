3e3a90afd1eff828a9fa8038e2bb615ec14b9bbf
Felipe Jardim <flipmode011@gmail.com>
2015-06-04 09:16:01 -0700

    new feature and bug 404 page

--- simple_rate_card/simple_RC.js
+++ simple_rate_card/simple_RC.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-	/* temp */
-	alert("This is still a WIP...so no, it wont work 100% properly yet...")
-
 	/* toggle control panel */
 	$(".input-toggle").click(function() {
 		$(".user_inputs").toggleClass("open-panel", 400)
@@ -18,47 +15,160 @@ $(document).ready(function() {
 		document.getElementById("form-inputs").reset()
 	});
 
-	/* define rate card values 
-		twitter base (distro) = 6.18
-		insta base (distro) = 9.10
-		face base (distro) = 5.20
+	/* define constant variables */
+	distCPMtwit = 6175/1000;
+	creatCPMtwit = 10969/1000;
+	distCPMinst = 9100/1000;
+	creatCPMinst = 13500/1000;
+	distCPMface = 5200/1000;
+	creatCPMface = 8438/1000;
+	totalProdVal = 0;
+	totalAddVal = 0;
+	totalClientCost = 0;
+	totalECPM = 0;
+	totalImpressions = 0;
 
-		creation premium = *1.375
+	/* function to add or remove currency format */
+	addMoneyFormat = function(obj) {
+		return obj.toString().replace(/^\b/g, "$").replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+	};
+	addThousandsFormat = function(obj) {
+		return obj.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+	};
+	removeMoneyFormat = function(obj) {
+		return +obj.toString().replace(/\$/g, "").replace(/,/g, "");
+	};  
 
-		must add click function to store a user input value?
+	/*  define function for calculating and displaying RC totals */
+	calcTotalRC = function() {
+		$(".RC_output #prodVal").each(function() {
+			totalProdVal += removeMoneyFormat($(this).text());
+		});
+		$(".RC_output #addVal").each(function() {
+			totalAddVal += removeMoneyFormat($(this).text());
+		});
+		$(".RC_output #impressions").each(function() {
+			totalImpressions += removeMoneyFormat($(this).text());
+		});
+		totalClientCost = totalProdVal - totalAddVal;
+		totalECPM = totalClientCost/totalImpressions*1000;
 
-	*/
-	twitter = 618/100;
-	instagram = 910/100;
-	facebook = 520/100;
-	contentDistro = 1;
-	contentCreate = 1375/1000;
-	prodVal = $("#prodValInput").val();
-	addVal = $("#addValInput").val();
+		$(".RC_output #totalProdVal").text(addMoneyFormat(totalProdVal.toFixed(2)));
+		$(".RC_output #totalAddVal").text(addMoneyFormat(totalAddVal.toFixed(2)));
+		$(".RC_output #totalClientCost").text(addMoneyFormat(totalClientCost.toFixed(2)));
+		$(".RC_output #totalECPM").text(addMoneyFormat(totalECPM.toFixed(2)));
+		$(".RC_output #totalImpressions").text(addThousandsFormat(totalImpressions));
 
+		totalProdVal = 0;
+		totalAddVal = 0;
+		totalClientCost = 0;
+		totalECPM = 0;
+		totalImpressions = 0;
+	};
 
 	/* adding lines to the rate card */
 	$("#add-to-rc").click(function() {
+		/* set platform choices */
+		twitter_chk = $("#selectPlatTwit").prop("checked");
+		instagram_chk = $("#selectPlatInsta").prop("checked");
+		facebook_chk = $("#selectPlatFb").prop("checked");
+		if ((twitter_chk || instagram_chk || facebook_chk) === false) {
+			alert("You must select at least 1 platform");
+			return;
+		};
+		platSelection = [];
+		distCPMs = [];
+		creatCPMs = [];
+		var i = 0;
+		if (twitter_chk) {
+			platSelection[i] = " Twitter";
+			distCPMs[i] = distCPMtwit;
+			creatCPMs[i] = creatCPMtwit;
+			i++;
+		};
+		if (instagram_chk) {
+			platSelection[i] = " Instagram";
+			distCPMs[i] = distCPMinst;
+			creatCPMs[i] = creatCPMinst;
+			i++;
+		};
+		if (facebook_chk) {
+			platSelection[i] = " Facebook";
+			distCPMs[i] = distCPMface;
+			creatCPMs[i] = creatCPMface;
+			i++;
+		};
+
+		/* set product choices */
+		if ($("#selectProdDistro").prop("checked")) {
+			prodChoice = "Content Distribution";
+			prodCPMs = distCPMs;
+		} else {
+			prodChoice = "Content Creation";
+			prodCPMs = creatCPMs;
+		};
+
+		blendCPM = 0;
+		for (var j = 0; j < prodCPMs.length; j++) {
+			blendCPM += prodCPMs[j]/prodCPMs.length;
+		};
+
+		/* store budget inputs */
+		prodVal = $("#prodValInput").val();
+		prodVal = +prodVal.replace(/,/g, "");
+		addVal = $("#addValInput").val();
+		addVal = +addVal.replace(/,/g, "");
+		if ((prodVal == "" || isNaN(prodVal)) && isNaN(addVal)) {
+			alert("Please make sure your Product and Added Value are valid numbers. Product Value cannot be 0");
+				$("#prodValInput").val("50000");
+				$("#addValInput").val("0");
+				return;
+		};
+		if (prodVal == "" || isNaN(prodVal)) {
+			alert("Please make sure your Product Value is greater than 0 and is a valid number");
+			$("#prodValInput").val("50000");
+			return;
+		};
+		if (isNaN(addVal)) {
+			alert("Please make sure your Added Value is a valid number");
+			$("#addValInput").val("0");
+			return;
+		};
+
+		/* calculate remaining values */
+		clientCost = prodVal-addVal;
+		impressions = prodVal*1000/blendCPM;
+		eCPM = clientCost/(impressions/1000);
+
+		/* writing lines to the table */
 		$(".RC_output tbody").append(
 			"<tr>" +
-				"<td>Product</td>" +
-				"<td>Platform</td>" +
-				"<td>Prod Val</td>" +
-				"<td>A.V.</td>" +
-				"<td>client cost</td>" +
-				"<td>eCPM</td>" +
-				"<td>TR</td>" +
+				"<td>" + prodChoice + "</td>" +
+				"<td>" + platSelection + "</td>" +
+				"<td id='prodVal'>" + addMoneyFormat(prodVal.toFixed(2)) + "</td>" +
+				"<td id='addVal'>" + addMoneyFormat(addVal.toFixed(2)) + "</td>" +
+				"<td>" + addMoneyFormat(clientCost.toFixed(2)) + "</td>" +
+				"<td>" + addMoneyFormat(eCPM.toFixed(2)) + "</td>" +
+				"<td id='impressions'>" + addThousandsFormat(impressions.toFixed(0)) + "</td>" +
 				"<td><a href='#' class='remove-row-btn'><span class='ui-icon ui-icon-minusthick'></span></a></td>" +
 			"</tr>"
-		)
+		);
+
+		calcTotalRC();
 	});
 
 	/* remove rows from rate card */
+	// clear table
 	$(".tb-btn-group #clear-tb").click(function() {
-		$("tbody tr").remove()
+		$("tbody tr").remove();
+		$(".RC_output tfoot td:not(:nth-child(2))").empty();	
 	});
+	// remove row
 	$("tbody").on("click", ".remove-row-btn", function() {
-		$(this).parent().parent().remove()
+		$(this).parent().parent().remove();
+		calcTotalRC();
+		if (+$(".RC_output #totalImpressions").text() == 0)
+			$(".RC_output tfoot td:not(:nth-child(2))").empty();
 	});
 
 	/* download to excel */
